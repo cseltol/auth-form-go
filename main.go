c98ac93b3378c9b71a061d7c2f689c1d8b10a169
cseltol <64264529+Xeltol@users.noreply.github.com>
2021-02-03 17:53:23 +0700

    add html page later

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type UserDetails struct {
+	Login string
+	Password string
+	Success bool
+	StorageAccess string
+}
+
+var (
+	tmpl = template.Must(template.ParseFiles("forms.html"))
+)
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	data := UserDetails {
+		Login: req.FormValue("login"),
+		Password: req.FormValue("password"),
+	}
+	data.Success = true // если юзер уже есть в базе иначе false
+	tmpl.Execute(w, data)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":4000", nil))
+}
